8fdddb535c217a527e248cd6f25385710f3559a6
Thiago Silva <thiago@metareload.com>
2013-08-09 18:59:24 -0300

    Adding example_runtime.py

---
+++ examples/example_runtime.py
@@ -0,0 +1,33 @@
+from pymeta.grammar import OMeta
+from pymeta.builder import TreeBuilder, moduleFromGrammar
+from pymeta.grammar import OMetaGrammar
+from pymeta.runtime import _MaybeParseError, OMetaBase, EOFError
+
+## parsing text...
+
+grammar = """
+start = spaces number:l operator:op number:r -> [op, l, r]
+number = spaces digit+:d -> int(''.join(d))
+operator = token("+") | token("-")
+"""
+
+Grammar = OMeta.makeGrammar(grammar, {})
+parser = Grammar("5 + 7")
+ast, err =  parser.apply("start")
+
+print "AST: " + str(ast)
+
+## parsing list with `ast` as input...
+
+grammar_ast = """
+start = ['+' number:a number:b] -> a + b
+      | ['-' number:a number:b] -> a - b
+number = :a
+"""
+
+ASTGrammar = OMetaGrammar(grammar_ast)
+t = ASTGrammar.parseGrammar('SomeGrammarName', TreeBuilder)
+ASTParser = moduleFromGrammar(t, 'SomeGrammarName', OMetaBase, {})
+parser = ASTParser([ast])
+res, err = parser.apply("start")
+print "result: " + str(res)
